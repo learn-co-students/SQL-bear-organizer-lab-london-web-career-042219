b2bfe7b40bb63a17e6834bd2b547ecdee2b5a498
MarcusMellorMIM <marcus@mellor.com>
2019-05-01 14:50:29 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,22 @@
+INSERT INTO bears ( name, age, gender, color, temperament, alive)
+VALUES  ("Mr. Chocolate",12,"Male","Brown","Calm",1);
+
+INSERT INTO bears ( name, age, gender, color, temperament, alive)
+VALUES  ("Rowdy",2,"Female","Black","Rowdy",1);
+
+INSERT INTO bears ( name, age, gender, color, temperament, alive)
+VALUES  ("Tabitha",7,"Female","White","Killer",1);
+
+INSERT INTO bears ( name, age, gender, color, temperament, alive)
+VALUES  ("Sergeant Brown",17,"Male","Brown","Calm",1);
+
+INSERT INTO bears ( name, age, gender, color, temperament, alive)
+VALUES  ("Melissa",7,"Female","Black","Calm",0);
+
+INSERT INTO bears ( name, age, gender, color, temperament, alive)
+VALUES  ("Grinch",12,"Female","Red","Cheeky",1);
+
+INSERT INTO bears ( name, age, gender, color, temperament, alive)
+VALUES  ("Wendy",17,"Female","Red","Magestic",0);
+
+INSERT INTO bears ( name) VALUES  (null);
